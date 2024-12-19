dcf5b56d0e51be0055ef50264f38138d5acbea15
Sushant Pulavarthi <sushant.pulavarthi09@gmail.com>
2024-12-19 16:24:18 +0000

    Refactor: Update plugin to newer IDE version

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2023.2.8")
+    version.set("2024.3.1")
     type.set("IC") // Target IDE Platform
 
     plugins.set(listOf(/* Plugin Dependencies */))
@@ -32,7 +32,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("232")
-        untilBuild.set("242.*")
+        untilBuild.set("243.*")
     }
 
     signPlugin {
--- src/main/kotlin/com/sushant/intellijsense/uihop/UIHop.kt
+++ src/main/kotlin/com/sushant/intellijsense/uihop/UIHop.kt
@@ -1,61 +1,50 @@
 package com.sushant.intellijsense.uihop
 
+import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.actionSystem.impl.ActionButton
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.wm.WindowManager
 import com.intellij.ui.JBColor
 import java.awt.Container
-import javax.swing.AbstractButton
 import javax.swing.JComponent
 import javax.swing.JLabel
 import javax.swing.JLayeredPane
+import javax.swing.JRootPane
 
 class UIHop : DumbAwareAction() {
-    private var active = false
+    private var highlightingActive = false
     private val highlightedComponents = mutableSetOf<JComponent>()
-    private var panel: JLayeredPane? = null
+    private var highlightPane: JLayeredPane? = null
 
     override fun actionPerformed(e: AnActionEvent) {
         val projectFrame = WindowManager.getInstance().getFrame(e.project) ?: return
         val rootPane = projectFrame.rootPane
 
-        if (active) {
+        // Create pane for all highlights to live on
+        if (highlightPane == null) createPanel(rootPane)
+
+        if (highlightingActive) {
             removeHighlights()
         } else {
-            if (panel == null) {
-                panel = JLayeredPane()
-                panel!!.layout = null
-                panel!!.bounds = rootPane.bounds
-                panel!!.size = rootPane.size
-                panel!!.isVisible = true
-                panel!!.isOpaque = false
-            }
-            if (panel!!.parent == null) {
-                rootPane.add(panel)
-            }
-            rootPane.setComponentZOrder(panel, 0)
-            highlightComponents(rootPane.layeredPane, panel!!)
+            highlightComponents(rootPane.layeredPane, highlightPane!!)
         }
-        panel?.repaint()
-        active = !active
-    }
 
-    override fun update(e: AnActionEvent) {
-        val project = e.getData(CommonDataKeys.PROJECT)
-        e.presentation.isEnabledAndVisible = project != null
+        // Update and redraw elements of highlight pane
+        highlightPane?.repaint()
+        highlightingActive = !highlightingActive
     }
 
     private fun highlightComponents(component: Container, rootPane: Container) {
         component.components.forEach {
-            if (it is JComponent && it.isVisible) {
-                if (it is AbstractButton) {
-                    val label = JLabel("Aa")
-                    label.foreground = JBColor.RED
-                    label.bounds = it.bounds
-                    rootPane.add(label)
-                    highlightedComponents.add(label)
-                }
+            if (it is JComponent && it.isVisible && it is ActionButton) {
+                val label = JLabel("Aa")
+                label.foreground = JBColor.RED
+                label.bounds = it.bounds
+
+                rootPane.add(label)
+                highlightedComponents.add(label)
             }
             if (it is Container) {
                 highlightComponents(it, rootPane)
@@ -68,5 +57,22 @@ class UIHop : DumbAwareAction() {
             it.parent.remove(it)
         }
         highlightedComponents.clear()
+    }
+
+    override fun update(e: AnActionEvent) {
+        val project = e.getData(CommonDataKeys.PROJECT)
+        e.presentation.isEnabledAndVisible = project != null
+    }
+
+    override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.EDT
+
+    private fun createPanel(rootPane: JRootPane) {
+        highlightPane = JLayeredPane()
+        highlightPane!!.bounds = rootPane.bounds
+        highlightPane!!.layout = null
+        highlightPane!!.isVisible = true
+        highlightPane!!.isOpaque = false
+        rootPane.add(highlightPane)
+        rootPane.setComponentZOrder(highlightPane, 0)
     }
 }
